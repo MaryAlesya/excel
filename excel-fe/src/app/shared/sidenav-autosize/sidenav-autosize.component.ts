b8eafd099cbd9079e509f0cb50aa7915abc6ef02
MaryAlesya <alesya.alesya1430@gmail.com>
2025-04-22 10:16:28 +0530

    Removing comments
    Removing comments

--- excel-fe/src/app/shared/sidenav-autosize/sidenav-autosize.component.ts
+++ excel-fe/src/app/shared/sidenav-autosize/sidenav-autosize.component.ts
@@ -64,8 +64,3 @@ export class SidenavAutosize {
   }
 
 }
-
-
-/**  Copyright 2017 Google Inc. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
